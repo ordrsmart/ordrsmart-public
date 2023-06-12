59b113999b5d8df1e2d99263c61fe0c3c5a9f909
Shubham Goenka <gshubham@berkeley.edu>
2023-06-12 15:21:34 +0530

    Update sample-application.js

--- samples/sample-application.js
+++ samples/sample-application.js
@@ -1,42 +1,45 @@
-// populateSampleApplicationCards populates a given container with the various sample application cards.
-function populateSampleApplicationCards(containerID) {
-  let apps = [
-    {
-      id: 'New-Product-Development',
-      text: "New Product Development",
-    },
-    {
-      id: 'Change-Supplier',
-      text: "Looking to Change Supplier",
-    },
-    {
-      id: 'Replace-Ingredient',
-      text: "Replacing Current Ingredient",
-    },
-    {
-      id: 'Other',
-      text: "Other",
-    },
-  ];
-  $.each(apps, function() {
-    let card = $('<label />', {
-      class: 'radio-button-field w-radio'                          
-    }).append(
-      $('<input />', { 
-        type: 'radio', 
-        'data-name': 'sample_application',
-        id: this.id,
-        name: 'sample_application',
-        value: this.id.replaceAll('-', ' '),
-        class: 'w-form-formradioinput sysf-radio-button w-radio-input'
-      })
-    ).append(
-      $('<span />', {
-        class: 'radiobuttonpillnarrow sample-application w-form-label',
-        for: this.id,
-        'text': this.text
-      })
-    );
-    $(containerID).append(card);
-  });
+class SampleApplication {
+  // populateSampleApplicationCards populates a given container with the various sample application cards.
+  function populateSampleApplicationCards(containerID) {
+    let apps = [
+      {
+        id: 'New-Product-Development',
+        text: "New Product Development",
+      },
+      {
+        id: 'Change-Supplier',
+        text: "Looking to Change Supplier",
+      },
+      {
+        id: 'Replace-Ingredient',
+        text: "Replacing Current Ingredient",
+      },
+      {
+        id: 'Other',
+        text: "Other",
+      },
+    ];
+    $.each(apps, function() {
+      let card = $('<label />', {
+        class: 'radio-button-field w-radio'                          
+      }).append(
+        $('<input />', { 
+          type: 'radio', 
+          'data-name': 'sample_application',
+          id: this.id,
+          name: 'sample_application',
+          value: this.id.replaceAll('-', ' '),
+          class: 'w-form-formradioinput sysf-radio-button w-radio-input'
+        })
+      ).append(
+        $('<span />', {
+          class: 'radiobuttonpillnarrow sample-application w-form-label',
+          for: this.id,
+          'text': this.text
+        })
+      );
+      $(containerID).append(card);
+    });
+  }
 }
+
