c5bc23c4c28a9704687017e9849f5e2fafc5a9f5
Shubham Goenka <gshubham@berkeley.edu>
2023-06-13 13:45:55 +0530

    Update utils.js
    Add `preventButtonClick` utility function

--- samples/utils.js
+++ samples/utils.js
@@ -1,3 +1,33 @@
+// preventButtonClick prevents a given button from being clicked
+// and displays the given warning
+// if any of the entries in a given list of values is undefined
+function preventButtonClick(buttonID, values, warningID) {
+  $(warningID).hide();
+  
+  $(buttonID).on('click', function(e) {
+    if (buttonID == '#submitButton') {
+      console.log('entered');
+      console.log('values', values);
+      console.log('warning ID', warningID);
+    }
+    let prevent = false;
+    values.forEach(v => {
+      if (buttonID == '#submitButton') {
+        console.log('current value is', v);
+      }
+      if (!v) {
+        prevent = true;
+        break;
+      }
+    });
+    
+    if (prevent) {
+      $(warningID).show();
+  	   e.stopPropagation();
+    }
+  });
+}
+
 // toTitleCase converts a string to Title Case
 function toTitleCase(str) {
  	return str.toLowerCase().split(' ').map(function (word) {
