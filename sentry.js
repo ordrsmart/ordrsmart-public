a6b69c0b0850fda62ed6668a8e573ffb9b8b2e69
Shubham Goenka <gshubham@berkeley.edu>
2023-06-27 18:44:32 +0530

    Update sentry.js
    Update based on JS SDK installation steps

--- sentry.js
+++ sentry.js
@@ -1,19 +1,9 @@
-import * as Sentry from "@sentry/browser";
-
-Sentry.init({
-  dsn: "https://f6d1ccddfc224996a1cc354db7882994@o4505431537156096.ingest.sentry.io/4505431543840768",
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+Sentry.onLoad(function() {
+  Sentry.init({
+    // No need to configure DSN here, it is already configured in the loader script
+    // You can add any additional configuration here
+    sampleRate: 0.5,
+  });
 });
 
-// myUndefinedFunction();
+myUndefinedFunction();
