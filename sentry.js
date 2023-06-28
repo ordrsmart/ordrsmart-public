8ddb12841b91da9ba36a989410d2c285a00359ac
Shubham Goenka <gshubham@berkeley.edu>
2023-06-28 18:53:05 +0530

    Update sentry.js
    Configure debug option for staging

--- sentry.js
+++ sentry.js
@@ -1,7 +1,14 @@
+// configure Sentry initialization options
+const initOptions = {
+  // No need to configure DSN here, it is already configured in the loader script
+  // You can add any additional configuration here
+  sampleRate: 0.5,
+};
+// edit configuration for staging env
+if (location.hostname == "ordrsmart.webflow.io") {
+  initOptions.debug = true;
+}
+
 Sentry.onLoad(function() {
-  Sentry.init({
-    // No need to configure DSN here, it is already configured in the loader script
-    // You can add any additional configuration here
-    sampleRate: 0.5,
-  });
+  Sentry.init(initOptions);
 });
