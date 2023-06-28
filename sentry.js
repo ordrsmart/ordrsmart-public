6c74af9ad6c993b89ff9da8712f555d36a1ae014
Shubham Goenka <gshubham@berkeley.edu>
2023-06-28 19:16:09 +0530

    Update sentry.js
    Configure session events tracking

--- sentry.js
+++ sentry.js
@@ -3,11 +3,16 @@ const initOptions = {
   // No need to configure DSN here, it is already configured in the loader script
   // You can add any additional configuration here
   environment: "production",
+  // send session events to Sentry
+  autoSessionTracking: true,
 };
 // edit configuration for staging env
 if (location.hostname == "ordrsmart.webflow.io") {
   initOptions.debug = true;
   initOptions.environment = "staging";
+  // don't send session events to Sentry in staging
+  // TODO(shubham): uncomment the following line after testing session events in staging
+  // initOptions.autoSessionTracking = false;
 }
 
 Sentry.onLoad(function() {
