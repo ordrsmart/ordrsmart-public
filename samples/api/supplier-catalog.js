57ab1f26b0fcbd46dcf838931f3e78ae173e2475
Shubham Goenka <gshubham@berkeley.edu>
2023-06-29 18:16:45 +0530

    Update supplier-catalog.js
    Capture API fetch error in Sentry

--- samples/api/supplier-catalog.js
+++ samples/api/supplier-catalog.js
@@ -8,13 +8,16 @@ class SupplierCatalogAPI {
       let response = await fetch(url, {
         method: 'GET',
         headers: {
-          'Authorization': 'Bearer pat7T8A2HtJnQVCMN.5f73c92b2891a02fd5f69ba01bf29e66467017afdd003ce5c4853e4fa5e501f3',
+          // 'Authorization': 'Bearer pat7T8A2HtJnQVCMN.5f73c92b2891a02fd5f69ba01bf29e66467017afdd003ce5c4853e4fa5e501f3',
+          'Authorization': 'Bearer invalidToken',
           'Content-Type': 'application/json',
         }
       });
       return await response.json();
     } catch (e) {
-      console.log(e);
+      const fetchError = new Error(`failed to fetch supplier catalog for supplier with id ${supplierID}`);
+      fetchError.cause = e;
+      Sentry.captureException(fetchError);
     }
   }
 }
