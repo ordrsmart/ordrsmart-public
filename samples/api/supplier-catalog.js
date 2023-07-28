a9558043e31a5e86027780a427c7dedc93fcbfcf
Shubham Goenka <gshubham@berkeley.edu>
2023-07-28 20:56:59 +0530

    Update supplier-catalog.js
    Undo fake auth

--- samples/api/supplier-catalog.js
+++ samples/api/supplier-catalog.js
@@ -10,8 +10,7 @@ class SupplierCatalogAPI {
       let response = await fetch(url, {
         method: 'GET',
         headers: {
-          // 'Authorization': 'Bearer pat7T8A2HtJnQVCMN.5f73c92b2891a02fd5f69ba01bf29e66467017afdd003ce5c4853e4fa5e501f3',
-          'Authorization': 'blah',
+          'Authorization': 'Bearer pat7T8A2HtJnQVCMN.5f73c92b2891a02fd5f69ba01bf29e66467017afdd003ce5c4853e4fa5e501f3',
           'Content-Type': 'application/json',
         }
       });
