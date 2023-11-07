34c49bfc9ae1e9a7433850aad8ed6a164b215d40
Shubham Goenka <gshubham@berkeley.edu>
2023-11-06 22:11:23 -0600

    Update sample-fulfillment.js
    Populate with code

--- samples/sample-fulfillment.js
+++ samples/sample-fulfillment.js
@@ -1,1 +1,46 @@
-
+class SampleFulfillment {
+  // populateSampleFulfillmentCards populates a given container with the various sample fulfillment cards.
+  populateSampleFulfillmentCards(containerID) {
+    let apps = [
+      {
+        id: 'in-person-pickup',
+        text: "In-Person Pickup",
+      },
+      {
+        id: 'delivery',
+        text: "Deliver to my Address",
+      },
+    ];
+    
+    $.each(apps, function() {
+      let card = $('<label />', {
+        class: 'radio-button-field w-radio'                          
+      }).append(
+        $('<input />', { 
+          type: 'radio', 
+          'data-name': 'sample_fulfillment',
+          id: this.id,
+          name: 'sample_fulfillment',
+          value: this.id.replaceAll('-', ' '),
+          class: 'w-form-formradioinput sysf-radio-button w-radio-input'
+        })
+      ).append(
+        $('<span />', {
+          class: 'radiobuttonpillnarrow sample-fulfillment w-form-label',
+          for: this.id,
+          'text': this.text
+        })
+      );
+      
+      $(containerID).append(card);
+    });
+    
+    // set the ID field depending on the sample fulfillment option selected
+    let sampleFulfillmentCards = document.getElementsByName("sample_fulfillment");
+    sampleFulfillmentCards.forEach(card => {
+      card.addEventListener("change", function() {
+	      selectedSampleFulfillmentOptionID = card.id;
+      });
+    });
+  }
+}
